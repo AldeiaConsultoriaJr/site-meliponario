89e86e2bebe16d56015d894c07468d29668723ef
rodrigoAMF <rodrigoamf@outlook.com>
2019-02-19 14:15:40 -0300

    Update Galeria
    Agora a página detecta as imagens com extensão .jpg, .png, .gif que estão na pasta "img/galeria/" e insere automaticamente na galeria. 
    
    Adicionado evento onClick nas imagens da galeria. Quando uma imagem recebe o evento "click do mouse", a imagem é  exibida em fullscreen, utilizando um modal por cima da página.
    
    Adicionado mais imagens à galeria.
    
    Removido algumas imagens repetidas da galeria

--- galeria.php
+++ galeria.php
@@ -3,6 +3,8 @@
 	require_once 'functions.php';
 
 	includeHeader();
+
+	$_SESSION['contador'] = 1;
 ?>
 
 <section id="hero">
@@ -13,25 +15,77 @@
     </div>
 </section>
 
+<!-- The Modal -->
+<div id="myModal" class="modal">
+	<span class="close">&times;</span>
+	<img class="modal-content" id="imgModal">
+	<div id="caption"></div>
+</div>
+
 <section id="galeria-fotos">
     <div class="container">
-        <div class="row especial">
-            <div class="col-md-4" style="padding:0"><div class="um img-full"></div></div>
-            <div class="col-md-4" style="padding:0"><div class="dois img-full"></div></div>
-            <div class="col-md-4" style="padding:0"><div class="tres img-full"></div></div>
-        </div>
-        <div class="row especial">
-            <div class="col-md-4" style="padding:0"><div class="quatro img-full"></div></div>
-            <div class="col-md-4" style="padding:0"><div class="cinco img-full"></div></div>
-            <div class="col-md-4" style="padding:0"><div class="seis img-full"></div></div>
-        </div>
-        <div class="row especial">
-            <div class="col-md-4" style="padding:0"><div class="sete img-full"></div></div>
-            <div class="col-md-4" style="padding:0"><div class="oito img-full"></div></div>
-            <div class="col-md-4" style="padding:0"><div class="nove img-full"></div></div>
-        </div>
+		<?php
+		$files = glob('img/galeria/*.{jpg,png,gif}', GLOB_BRACE);
+		$primeiro = true;
+		foreach($files as $file) {
+			$id = explode("/", $file);
+			$id = explode(".", $id[2]);
+			$id = $id[0];
+			if($file == 'img/galeria/bg.jpg')
+				continue;
+			if($primeiro){
+				echo '<div class="row especial">' . "\n";
+			}
+			echo "\t\t\t" . '<div class="col-md-4" style="padding:0"><img id="imgGaleria' . $id . '"src="' . $file . '" class="img-full"></div>' . "\n";
+
+			if($_SESSION['contador']%3 == 0 && !$primeiro){
+				echo "\t\t" . '</div>' . "\n";
+				echo "\t\t" . '<div class="row especial">' . "\n";
+			}
+			if($primeiro){
+				$primeiro = false;
+			}
+			$_SESSION['contador']++;
+		}
+		$_SESSION['contador']--;
+		?>
     </div>
 </section>
+
+<script>
+	// Get the modal
+	var modal = document.getElementById('myModal');
+
+	var img = Array();
+
+	// Get the image and insert it inside the modal - use its "alt" text as a caption
+	<?php
+		for($i = 1; $i <= $_SESSION['contador']; $i++){
+			echo "\t" . 'img[' . ($i-1) . '] = document.getElementById("imgGaleria' . $i . '");' . "\n";
+
+			echo "\t" . 'img[' . ($i-1) . '].onclick = function(){' . "\n";
+			echo "\t\t" . 'modal.style.display = "block";' . "\n";
+			echo "\t\t" . 'modalImg.src = this.src;' . "\n";
+			echo "\t" . '}' . "\n";
+		}
+	?>
+	var img = document.getElementById('imgGaleria1');
+	var modalImg = document.getElementById("imgModal");
+	//var captionText = document.getElementById("caption");
+	img.onclick = function(){
+	  modal.style.display = "block";
+	  modalImg.src = this.src;
+	  //captionText.innerHTML = this.alt;
+	}
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	  modal.style.display = "none";
+	}
+</script>
 
 <?php
 	include(FOOTER_TEMPLATE);
